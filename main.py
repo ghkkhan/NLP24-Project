6c658694fe8a7f4b62f6a424671afe4b52e279c9
jme14 <williamsltrey3@gmail.com>
2024-11-19 10:54:26 -0600

    refactor for easier reading

---
+++ main.py
@@ -0,0 +1,15 @@
+# the idea here is to either use stuff from the movie_reviews corpus OR 
+# from the IMDB collection of text 
+from spacy_test import get_films_from_review
+
+import sys 
+
+if __name__ == "__main__":
+    # holds corpus or imdb 
+    source = sys.argv[1]
+
+    print(source)
+    if (source == "corpus" or source == "0"):
+        print(get_films_from_review(source="corpus")) 
+    elif source == "imdb" or  source == "1":
+        print(get_films_from_review(source="imdb")) 
--- spacy_test.py
+++ spacy_test.py
@@ -9,45 +9,72 @@ from nltk import word_tokenize, pos_tag, ne_chunk # type: ignore
 
 from tmdb_api import get_common_films_from_name_array
 
-male_names = names.words("male.txt")
-female_names = names.words("female.txt")
-all_names_capitalized = set(male_names + female_names)
-all_names = {name.lower() for name in all_names_capitalized}
 
-all_files = movie_reviews.fileids()
-the_file = random.choice(all_files)
+def get_all_names():
+    male_names = names.words("male.txt")
+    female_names = names.words("female.txt")
+    all_names_capitalized = set(male_names + female_names)
+    all_names = {name.lower() for name in all_names_capitalized}
+    return all_names
+    
 
-review = " ".join(list(movie_reviews.words(the_file)))
+def get_corpus_text():
+    all_files = movie_reviews.fileids()
+    the_file = random.choice(all_files)
+    review = " ".join(list(movie_reviews.words(the_file)))
+    return review
+
+def get_imdb_text():
+    
+
+def get_name_array(review_text):
+    # SPACY-TUTORIAL: https://www.wisecube.ai/blog/named-entity-recognition-ner-with-python/
+
+    # define the named entity categories that we want to recognize 
+    ner_categories = ['PERSON', "ORG", "GPE", "PRODUCT"]
+
+    doc = nlp(review_text)
+    #print(review)
+    entities = []
+    for ent in doc.ents:
+        if ent.label_ in ner_categories:
+            entities.append((ent.text, ent.label_))
+
+    name_array = []
+    for entity, category in entities:
+        print(f"{entity}: {category}")
+        name_array.append(entity)
+    return name_array
 
 
 
-# SPACY-TUTORIAL: https://www.wisecube.ai/blog/named-entity-recognition-ner-with-python/
+def get_films_from_review(source):
+    
+    all_names = get_all_names()
 
-# define the named entity categories that we want to recognize 
-ner_categories = ['PERSON', "ORG", "GPE", "PRODUCT"]
+    review_text = ""
+    if (source == "corpus"):
+        # this will change depending on the argument provided 
+        review_text = get_corpus_text()
+    elif (source == "imdb"):
+        review_text = get_corpus_text()
+        
 
-doc = nlp(review)
-#print(review)
-entities = []
-for ent in doc.ents:
-    if ent.label_ in ner_categories:
-        entities.append((ent.text, ent.label_))
+    name_array = get_name_array(review_text)
 
-entityset = set(entities)
-name_array = []
-for entity, category in entities:
-    print(f"{entity}: {category}")
-    name_array.append(entity)
+    #print(movie_reviews.raw(the_file))
+    print(review_text)
+    print("The above review is of one of these possibilities:")
+    #print(potential_titles)
 
-print(f"Formattable names detected: {len(name_array)}")
-potential_titles = get_common_films_from_name_array(name_array)
+    print(f"Formattable names detected: {len(name_array)}")
+    potential_titles = get_common_films_from_name_array(name_array)
 
 
-print(movie_reviews.raw(the_file))
-print("The above review is of one of these possibilities:")
-#print(potential_titles)
 
-for index, title in enumerate(potential_titles):
-    if index > 10:
-        break
-    print(f"{title["Title"]} ({title["Appears"]})")
+    answer_strings = []
+    for index, title in enumerate(potential_titles):
+        if index > 10:
+            break
+        answer_strings.append(f"{title["Title"]} ({title["Appears"]})")
+    return answer_strings
